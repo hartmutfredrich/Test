87986a6b010b172bd2fc36e9ebe30b8311b20953
Hartmut Fredrich <hartmut@gimli.fritz.box>
2014-06-19 19:36:08 +0200

    Initial Commit

---
+++ test/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  test
+//
+//  Created by Hartmut Fredrich on 19.06.14.
+//  Copyright (c) 2014 Hartmut Fredrich. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
